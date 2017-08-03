287856d8526610308ee63a77c26a61e463f8e6d8
Toma Zelić <zelic.toma@gmail.com>
2017-08-03 18:12:09 +0200

    Connect to db and create tables

--- server/db/index.js
+++ server/db/index.js
@@ -1,22 +1,44 @@
-const Sequelize = require('sequelize');
 const dbConfig = require('config').get('database');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 
 const log = require('../logger').serverLogger;
 
 const { dialect, host, user, password, name, pool } = dbConfig;
 
-const connection = new Sequelize(name, user, password, {
-  host,
-  dialect,
-  pool
-});
+const db = {
+  Sequelize,
+  sequelize: new Sequelize(name, user, password, { host, dialect, pool }),
+  models: {}
+};
 
-connection.authenticate()
+function importModel(file) {
+  const model = db.sequelize.import(path.join(__dirname, file));
+  db.models[model.name] = model;
+  return model;
+}
+
+fs.readdirSync(__dirname)
+  .filter(file => file !== 'index.js')
+  .map(importModel)
+  .filter(model => model.associate)
+  .map(model => model.associate(db.models));
+
+db.sequelize.authenticate()
   .then(() => {
-    log.info('Connection has been established successfully.');
+    log.info('Connection to database has been established successfully.');
   })
   .catch(err => {
     log.error('Unable to connect to the database:', err);
   });
 
-module.exports = connection;
+db.sequelize.sync()
+  .then(() => {
+    log.info('Tables synced successfully.');
+  })
+  .catch(err => {
+    log.error('Unable to sync database tables:', err);
+  });
+
+module.exports = db;
