49923652670669550548be6c5c4b81505af11219
Toma Zelić <zelic.toma@gmail.com>
2017-08-03 18:11:57 +0200

    Create Device and User models

---
+++ server/db/Device.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('Device', {
+    cat_num: {
+      type: DataTypes.STRING,
+      validate: {
+        isNumeric: true,
+        len: 4,
+        min: '0000',
+        max: '9999'
+      },
+      unique: true
+    },
+    make: DataTypes.STRING,
+    model: DataTypes.STRING,
+    os: DataTypes.STRING,
+    serial: DataTypes.STRING
+  });
+
+  User.associate = models => {
+    User.belongsTo(models.User);
+  };
+
+  return User;
+};
