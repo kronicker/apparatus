363c088ec023fc0d9a821b89652fc9a7d2381516
Toma Zelić <zelic.toma@gmail.com>
2017-10-10 16:04:09 +0200

    Add basic CRUD endpoints for Liabilities

--- server/routes/liability.js
+++ server/routes/liability.js
@@ -1,10 +1,53 @@
 const router = require('express').Router();
-const { Liability } = require('../db');
+const { Liability } = require('../db').models;
 
-function check(req, res) {
-  res.send('It works!!');
+const DEFAULT_LIMIT = 30;
+
+function create(req, res, next) {
+  return Liability.create(req.body)
+    .then(newLiability => res.send(newLiability))
+    .catch(err => next(err));
 }
 
-router.get('/liability', check);
+function get(req, res, next) {
+  const id = req.params.id;
+
+  return Liability.findById(id)
+    .then(liability => res.send(liability))
+    .catch(err => next(err));
+}
+
+function list(req, res, next) {
+  const limit = Number(req.query.limit) || DEFAULT_LIMIT;
+  const offset = Number(req.query.offset) || 0;
+
+  return Liability.findAll({ limit, offset })
+    .then(liabilities => res.send(liabilities))
+    .catch(err => next(err));
+}
+
+function remove(req, res, next) {
+  const id = req.params.id;
+
+  const where = { id };
+  return Liability.destroy({ where })
+    .then(() => res.end())
+    .catch(err => next(err));
+}
+
+function update(req, res, next) {
+  const id = req.params.id;
+
+  return Liability.findById(id)
+    .then(liability => liability.update(req.body))
+    .then(liability => res.send(liability))
+    .catch(err => next(err));
+}
+
+router.get('/liability', list);
+router.post('/liability', create);
+router.get('/liability/:id', get);
+router.delete('/liability/:id', remove);
+router.put('/liability/:id', update);
 
 module.exports = router;
